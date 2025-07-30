4d289ed3b752491ec7d1a59308ad96d3d980f108
Maksymilian Stolarski <max@stolarski.info>
2025-07-30 21:49:59 +0200

    Initial Commit

---
+++ EscapePlugin/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+
+namespace EscapePlugin
+{
+    public class EscapePlugin : Plugin<Config>
+    {
+        public override string Name => "EscapePlugin";
+        public override string Author => "TheSheosi, Shibusekk";
+        public override Version Version => new Version(1, 0, 1);
+        public override Version RequiredExiledVersion => new Version(9, 6, 1);
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.Escaping += OnEscaping;
+            Log.Info("EscapePlugin has been enabled. Thank you for using our plugin.");
+        }
+
+        private void OnEscaping(EscapingEventArgs ev)
+        {
+            ev.NewRole = RoleTypeId.Spectator;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# EscapePlugin
+
+## What's EscapePlugin
+**EscapePlugin** is a plugin for [EXILED](https://github.com/ExMod-Team/EXILED) that changes a role after escape to spectator.
